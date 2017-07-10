3569ac560eb894e1a7ce7013035ff7199918dd52
Jee Peng Lewis Tham <s10171744@connect.np.edu.sg>
2017-07-10 20:22:22 +0800

    retrofit

--- app/src/main/java/s10171744d/rwethereyet/MainActivity.java
+++ app/src/main/java/s10171744d/rwethereyet/MainActivity.java
@@ -3,11 +3,20 @@ package s10171744d.rwethereyet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.github.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        GitHubService service = retrofit.create(GitHubService.class);
     }
 }
