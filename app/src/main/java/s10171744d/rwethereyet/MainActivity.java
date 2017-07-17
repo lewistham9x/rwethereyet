b482366cd2e209e38e28cbe50159c0265fdb8e1a
Jee Peng Lewis Tham <s10171744@connect.np.edu.sg>
2017-07-17 20:23:12 +0800

    before i turn servicelist into a method for optimisation

--- app/src/main/java/s10171744d/rwethereyet/MainActivity.java
+++ app/src/main/java/s10171744d/rwethereyet/MainActivity.java
@@ -29,11 +29,12 @@ import s10171744d.rwethereyet.util.SingleArgumentCallback;
 
 public class MainActivity extends AppCompatActivity {
     List<BusStop> BusStopList;
-    ListView busRouteListView;
+    ListView busRouteListView1;
+    ListView busRouteListView2;
     EditText busServiceNo;
     ToggleButton btnToggleRoute;
 
-    Integer routeno;
+    Integer routecount;
 
     List<BusStop> selectedServiceBusStopList;
 
@@ -42,11 +43,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        routeno=1;
-
         final Button queryButton = (Button) findViewById(R.id.queryButton);
         final EditText busServiceNo = (EditText) findViewById(R.id.txtServiceNo);
-        busRouteListView = (ListView)findViewById(R.id.busRouteListView);
+        busRouteListView1 = (ListView)findViewById(R.id.busRouteListView1);
+        busRouteListView2 = (ListView)findViewById(R.id.busRouteListView2);
         btnToggleRoute = (ToggleButton)findViewById(R.id.btnToggleRoute);
 
         selectedServiceBusStopList = new ArrayList<>();
@@ -63,23 +63,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btnToggleRoute.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked)
-                {
-                    routeno = 2;
-                }
-                else
-                {
-                    routeno = 1;
-                }
-            }
-        });
-
 
         queryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                if (routecount == 1)
+                {
+
+                }
+                else if (routecount == 2)
+                {
+
+                }
+                else if (routecount == 3)
+                {
+
+                }
+
                 getBusStopList(busServiceNo.getText()+"", routeno, new SingleArgumentCallback<List<BusStop>>() {//call the callback
                     @Override
                     public void onComplete(final List<BusStop> serviceBusStopList) //will execute after callback is complete with data etc
@@ -88,8 +89,8 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
                 BusRouteListViewAdapter adapter = new BusRouteListViewAdapter(selectedServiceBusStopList);
-                busRouteListView.setAdapter(adapter);
-                busRouteListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+                busRouteListView1.setAdapter(adapter);
+                busRouteListView1.setOnItemClickListener(new AdapterView.OnItemClickListener(){
                     @Override
                     public void onItemClick(AdapterView<?> adapterView, View view, int position, long l)
                     {
@@ -103,57 +104,78 @@ public class MainActivity extends AppCompatActivity {
     }
     private void getBusStopList(String busService, final int routeNo, final SingleArgumentCallback<List<BusStop>> callback) //returns list of bus stops for service
     {
-       Network.getBusRouterService().listRepos(busService).enqueue(new Callback<BusRouterServiceResponse>() {//enqueue allows for usage of own callback
-           List<BusStop> busStopsForService = new ArrayList<>();
-           @Override
-           public void onResponse(Call<BusRouterServiceResponse> call, Response<BusRouterServiceResponse> response) {
-               if (response.isSuccessful()) //if response is successful (such a bus stop exists)
-               {
-                   BusRouterServiceResponse yay = response.body();
-                   String[] result = new String[0];
+        Network.getBusRouterService().listRepos(busService).enqueue(new Callback<BusRouterServiceResponse>() {//enqueue allows for usage of own callback
+            List<BusStop> busStopsForService = new ArrayList<>();
+            @Override
+            public void onResponse(Call<BusRouterServiceResponse> call, Response<BusRouterServiceResponse> response) {
+                if (response.isSuccessful()) //if response is successful (such a bus stop exists)
+                {
+                    BusRouterServiceResponse yay = response.body();
+                    String[] result = new String[0];
 
-                   switch (routeNo) { //neater way for if else
-                       case 1:
-                           result = yay.getRouteOne().getStops();
-                           break;
-                       case 2:
-                           result = yay.getRouteTwo().getStops();
-                           break;
-                       case 3:
-                           result = yay.getRouteThree().getStops();
-                           break;
-                   }
+                    //counting routes - need to count number of routes in order to display accordingly
+                    result = yay.getRouteOne().getStops();
+                    routecount = 0;
+                    if (result != null)
+                    {
+                        routecount++;
+                    }
+                    result = yay.getRouteTwo().getStops();
+                    routecount = 0;
+                    if (result != null)
+                    {
+                        routecount++;
+                    }
+                    result = yay.getRouteThree().getStops();
+                    routecount = 0;
+                    if (result != null)
+                    {
+                        routecount++;
+                    }
+                    //end of route counting
 
-                   for (String s : result)
-                   {
-                       for (BusStop bs : BusStopList)
-                       {
-                           if (bs.getCode().equals(s))
-                           {
-                               busStopsForService.add(bs);
-                               break;
-                           }
-                       }
-                   }
-               }
-               else
-               {
-                   //create error bus stop object for non existent bus service
-                   BusStop error = new BusStop();
-                   error.error(1);
-                   busStopsForService.add(error);
-               }
-               callback.onComplete(busStopsForService); //when retrieved network info "return" the busStopsForService
-           }
+                    switch (routeNo) { //neater way for if else
+                        case 1:
+                            result = yay.getRouteOne().getStops();
+                            break;
+                        case 2:
+                            result = yay.getRouteTwo().getStops();
+                            break;
+                        case 3:
+                            result = yay.getRouteThree().getStops();
+                            break;
+                    }
 
-           @Override
-           public void onFailure(Call<BusRouterServiceResponse> call, Throwable t) {
-               //create error bus stop object for no connection
-               BusStop error = new BusStop();
-               error.error(2);
-               busStopsForService.add(error);
-               callback.onComplete(busStopsForService); //when retrieved network info "return" the busStopsForService
-           }
-       });
+                    for (String s : result)
+                    {
+                        for (BusStop bs : BusStopList)
+                        {
+                            if (bs.getCode().equals(s))
+                            {
+                                busStopsForService.add(bs);
+                                break;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    //create error bus stop object for non existent bus service
+                    BusStop error = new BusStop();
+                    error.error(1);
+                    busStopsForService.add(error);
+                }
+                callback.onComplete(busStopsForService); //when retrieved network info "return" the busStopsForService
+            }
+
+            @Override
+            public void onFailure(Call<BusRouterServiceResponse> call, Throwable t) {
+                //create error bus stop object for no connection
+                BusStop error = new BusStop();
+                error.error(2);
+                busStopsForService.add(error);
+                callback.onComplete(busStopsForService); //when retrieved network info "return" the busStopsForService
+            }
+        });
     }
 }
